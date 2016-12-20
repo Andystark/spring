eaf9e6ee728dabfbec2d692b237706f884624a7e
Andystark <125537082@qq.com>
2016-12-20 19:55:48 +0800

    just test gitHub commit

--- src/main/java/com/songyj/DemoApplication.java
+++ src/main/java/com/songyj/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	//just for gitHub commit
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
